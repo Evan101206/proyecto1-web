04468a66d8c062f339adf1a6a723d402590a4ade
Evan101206 <murilloarleth21@gmail.com>
2025-01-28 13:42:19 -0600

    HTML
    se agrego carpeta html que contiene el index.htm

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+//obtener el boton por su id que e puse
+const botton=document.getElementById('botoncito');
+//agregar un evento al boton
+botton.addEventListener('click',function(){
+    alert("hola ya le diste click");
+});
